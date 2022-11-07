c82f9000774ec5763013763110a02f42553d2881
amrinlong <programrin2020@gmail.com>
2022-11-07 12:58:59 -0800

    first commit for excercise 26

---
+++ 26. Remove Duplicates from Sorted Array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = nums => {
+    let obj = {}
+    let results = []
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let key in obj) {
+            if (!obj[nums[i]]) {
+                console.log(obj[nums[i]] = nums[i])
+                console.log(obj.values[num[i]])
+            } 
+        }
+    }
+
+    //return
+};
--- twoSum.js
+++ twoSum.js
@@ -17,3 +17,5 @@ const arrOfNums = [2,7,11,15] // expected [0, 1]
 const targetNum = 9
 
 console.log(twoSum(arrOfNums, targetNum))
+
+// testing source control
